33707d04c38f1859cc3faaa52dd0c43c4c1c8f29
amir <amirabdulaziz01@gmail.com>
2025-04-16 17:37:41 +0800

    auto ping after 10 minutes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { swaggerUi, specs } = require("./Swagger");
+const fetch = require("node-fetch");
 
 dotenv.config();
 
@@ -35,7 +36,24 @@ app.use("/api", productRoutes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+  });
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+
+    // ping the server every 10 minutes
+  const SELF_URL = process.env.SELF_URL || `http://localhost:${PORT}`;
+  setInterval(() => {
+    fetch(`${SELF_URL}/ping`)
+      .then(res => res.text())
+      .then(data => console.log(`Self-ping: ${data}`))
+      .catch(err => console.error("Self-ping failed:", err));
+  }, 1000 * 60 * 10);
+
 });
+
+
